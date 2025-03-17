e1bd89ba78a6cc0efb03d98b9bea59ad16cf3de8
Ahmed ibrahim <Ahmed.ibrahim2867@gmail.com>
2025-03-17 07:19:13 +0200

    Updated project with new features/fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import AccountPage from "./pages/AccountPage";
-import PathsPage from "./pages/PathsPage";
 import PathDetails from "./pages/PathDetails";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/account" element={<AccountPage />} />
-        <Route path="/account/paths" element={<PathsPage />} />
         <Route path="/account/path/:id" element={<PathDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
